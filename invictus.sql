2d1ee2efbcb98eb8067035f5dc41eb2c1234f03a
Francesco Mirenda <61842200+shadowlinkct@users.noreply.github.com>
2024-05-23 17:09:16 +0200

    Add files via upload

--- invictus.sql
+++ invictus.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Creato il: Mag 19, 2024 alle 13:58
+-- Creato il: Mag 23, 2024 alle 17:08
 -- Versione del server: 10.4.28-MariaDB
--- Versione PHP: 8.2.4
+-- Versione PHP: 8.0.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -43,7 +43,34 @@ CREATE TABLE `account` (
 --
 
 INSERT INTO `account` (`nome`, `cognome`, `genere`, `data_n`, `email`, `password`, `tel_c`, `id`) VALUES
-('Francesco', 'Mirenda', 'uomo', '2024-05-17', 'francescomirenda1112@gmail.com', '$2y$10$tqqxxhLwtqzhNDBDz6iKiui.2akFwAIPrsxuwBU22dzpfoCy3Heta', '3484663564', 1);
+('Francesco', 'Mirenda', 'uomo', '2024-05-17', 'francescomirenda1112@gmail.com', '$2y$10$tqqxxhLwtqzhNDBDz6iKiui.2akFwAIPrsxuwBU22dzpfoCy3Heta', '3484663564', 1),
+('geppeto', 'sans', 'uomo', '2024-05-04', 'lucia18586@tisc', '$2y$10$t0JOBXtk2yRNRWJtU9GlBednsv5UttECuGrfnA4oWNTI8HWSIH4dq', '55484694129', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `articoli`
+--
+
+CREATE TABLE `articoli` (
+  `id` int(11) NOT NULL,
+  `data` date NOT NULL,
+  `titolo` varchar(255) NOT NULL,
+  `descrizione` text NOT NULL,
+  `link` varchar(255) DEFAULT NULL,
+  `immagine_segnalibro` varchar(255) DEFAULT '../img/bookmarkno.png'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `preferiti`
+--
+
+CREATE TABLE `preferiti` (
+  `id_utente` int(11) NOT NULL,
+  `id_articolo` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Indici per le tabelle scaricate
@@ -56,6 +83,19 @@ ALTER TABLE `account`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indici per le tabelle `articoli`
+--
+ALTER TABLE `articoli`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indici per le tabelle `preferiti`
+--
+ALTER TABLE `preferiti`
+  ADD PRIMARY KEY (`id_utente`,`id_articolo`),
+  ADD KEY `id_articolo` (`id_articolo`);
+
+--
 -- AUTO_INCREMENT per le tabelle scaricate
 --
 
@@ -63,7 +103,24 @@ ALTER TABLE `account`
 -- AUTO_INCREMENT per la tabella `account`
 --
 ALTER TABLE `account`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT per la tabella `articoli`
+--
+ALTER TABLE `articoli`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Limiti per le tabelle scaricate
+--
+
+--
+-- Limiti per la tabella `preferiti`
+--
+ALTER TABLE `preferiti`
+  ADD CONSTRAINT `preferiti_ibfk_1` FOREIGN KEY (`id_utente`) REFERENCES `account` (`id`),
+  ADD CONSTRAINT `preferiti_ibfk_2` FOREIGN KEY (`id_articolo`) REFERENCES `articoli` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
